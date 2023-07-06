c96a815094e14a4f07a26a53b76443583905566c
MarkGelo02 <138740413+MarkGelo02@users.noreply.github.com>
2023-07-06 15:21:10 +0800

    add some buttons

---
+++ app/src/main/java/com/example/priceverifier/AddStoreActivity.java
@@ -0,0 +1,26 @@
+package com.example.priceverifier;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AddStoreActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_addstore);
+
+        Button backButton = findViewById(R.id.backButton);
+
+        // Set click listener for the back button
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Finish the current activity and go back
+                finish();
+            }
+        });
+    }
+}
--- app/src/main/java/com/example/priceverifier/MainActivity.java
+++ app/src/main/java/com/example/priceverifier/MainActivity.java
@@ -14,6 +14,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        Button btnAddStore = findViewById(R.id.addStoreButton);
+        Button btnPriceVerify = findViewById(R.id.priceVerifierButton);
         Button btnImport = findViewById(R.id.importFileButton);
         btnImport.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -22,6 +25,22 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        btnPriceVerify.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, PriceVerifyActivity.class);
+                startActivity(intent);
+            }
+        });
+        btnAddStore.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AddStoreActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 
 }
