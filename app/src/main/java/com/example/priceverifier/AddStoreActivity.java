a20a281079087d08c87eae8982de60e186240954
AshleyTerese <ashleytereseb.31@gmail.com>
2023-07-10 11:29:58 +0800

    activity_priceverify.xml

--- app/src/main/java/com/example/priceverifier/AddStoreActivity.java
+++ app/src/main/java/com/example/priceverifier/AddStoreActivity.java
@@ -3,16 +3,25 @@ package com.example.priceverifier;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class AddStoreActivity extends AppCompatActivity {
+    private Button backButton;
+    private Button updateButton;
+    private Button saveButton;
+    private Button deleteButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_addstore);
 
         Button backButton = findViewById(R.id.backButton);
+        Button updateButton = findViewById(R.id.updateButton);
+        Button saveButton = findViewById(R.id.saveButton);
+        Button deleteButton = findViewById(R.id.deleteButton);
 
         // Set click listener for the back button
         backButton.setOnClickListener(new View.OnClickListener() {
@@ -22,5 +31,40 @@ public class AddStoreActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+        updateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                updateStore();
+            }
+        });
+
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                saveStore();
+            }
+        });
+
+        deleteButton.setOnClickListener((new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                deleteStore();
+            }
+        }));
+    }
+
+    private void updateStore() {
+        Toast.makeText(this, "Updated successfully", Toast.LENGTH_SHORT).show();
+    }
+
+    private void saveStore() {
+        Toast.makeText(this, "Saved successfully", Toast.LENGTH_SHORT).show();
+    }
+
+    private void deleteStore() {
+        Toast.makeText(this, "Deleted successfully", Toast.LENGTH_SHORT).show();
     }
 }
+
+
